d6c377312e207d6c5d2560b19a6a48665be95401
Kilian.Brandstoetter <kilian.brandstoetter@htl-wels.at>
2024-06-27 12:29:41 +0200

    feat: cors added

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import envUtil from './util/env.util';
 import express from 'express';
 import router from  './routes';
+import cors from 'cors'
 
 const {PORT} = envUtil;
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+
 app.use("/", router);
 
 app.listen(PORT,() => {
